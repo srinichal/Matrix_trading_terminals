ec45420ab3fd1f90f4c833918eadf1e16a856c2b
srinichal <srinichal.nal@gmail.com>
2026-08-04 07:46:17 +0530

    Update server.ts

--- server.ts
+++ server.ts
@@ -4,7 +4,7 @@ import { fileURLToPath } from 'url';
 import { createServer as createViteServer } from 'vite';
 
 const app = express();
-const PORT = 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 app.use(express.json());
 
